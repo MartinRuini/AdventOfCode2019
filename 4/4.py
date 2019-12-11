3f65ab9ccd5a395a0e492263b8e61a0bba2e4a39
Daniele Ruini <daniele.ruini@cern.ch>
2019-12-11 10:41:27 +0100

    finished 4 part 2

--- 4/4.py
+++ 4/4.py
@@ -18,26 +18,35 @@ def check_number(n):
 
 def check_number_partTwo(n):
   digit = {}
+  counter = {}
+  for i in range(10):
+    counter[i] = 0
+  double_digit = False
   for i in range(6):
     digit[i] = get_digit(n,i)
-  cons_same = False
-  for i in range(5):
-    if cons_same and digit[i]==digit[i+1]:
-      cons_same = False
-      continue
-    if digit[i]==digit[i+1]:
-      cons_same = True
-      break
-  if cons_same and (np.array(list(digit.values()))== np.sort(np.array(list(digit.values())))[::-1]).all():
+  for d in digit.values():
+    counter[d] += 1
+  if any(np.array(list(counter.values())) == 2):
+    double_digit = True
+  if double_digit and (np.array(list(digit.values()))== np.sort(np.array(list(digit.values())))[::-1]).all():
     return True
   return False
 
 input_range = (234208, 765869)
 
-good_numbers = []
-for n in range(*input_range):
-  if check_number(n):
-    good_numbers.append(n)
+#good_numbers = []
+#for n in range(*input_range):
+#  if check_number(n):
+#    good_numbers.append(n)
+#
+##print(good_numbers)
+#print(len(good_numbers))
 
-print(good_numbers)
-print(len(good_numbers))
+
+good_numbers_partTwo = []
+for n in range(*input_range):
+  if check_number_partTwo(n):
+    good_numbers_partTwo.append(n)
+
+print(good_numbers_partTwo)
+print(len(good_numbers_partTwo))
