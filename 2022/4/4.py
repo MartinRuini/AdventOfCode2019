a136e9ac68d6a57539e50bac74f3f00c1310b688
druini <daniele.ruini@cern.ch>
2022-12-04 11:35:20 +0100

    adding alternative 2022/4

--- 2022/4/4.py
+++ 2022/4/4.py
@@ -2,7 +2,6 @@ INPUT = 'input.txt'
 
 def main():
     complete_overlaps = 0
-    #partial_overlaps_strict  = 0
     partial_overlaps = 0
     with open(INPUT) as input_data:
         for line in input_data.readlines():
@@ -10,17 +9,15 @@ def main():
             first_assignment_start, first_assignment_stop = map(int, first_assignment.split('-'))
             second_assignment_start, second_assignment_stop = map(int, second_assignment.split('-'))
 
+            first_assignment_range  = range(first_assignment_start,first_assignment_stop+1)
+            second_assignment_range = range(second_assignment_start,second_assignment_stop+1)
+
             #part 1
-            if ((first_assignment_start <= second_assignment_start) and (first_assignment_stop >= second_assignment_stop)) or ((second_assignment_start <= first_assignment_start) and (second_assignment_stop >= first_assignment_stop)):
+            #if ((first_assignment_start <= second_assignment_start) and (first_assignment_stop >= second_assignment_stop)) or ((second_assignment_start <= first_assignment_start) and (second_assignment_stop >= first_assignment_stop)):
+            if all(first_section in second_assignment_range for first_section in first_assignment_range) or all(second_section in first_assignment_range for second_section in second_assignment_range):
                 complete_overlaps += 1
 
             #part 2
-#            if (second_assignment_start <= first_assignment_start <= second_assignment_stop) or (second_assignment_start <= first_assignment_stop <= second_assignment_stop):
-#            if (first_assignment_start <= second_assignment_start <= first_assignment_stop <= second_assignment_stop) or (second_assignment_start <= first_assignment_start <= second_assignment_stop <= first_assignment_stop):
-#                partial_overlaps_strict += 1
-#    partial_overlaps = complete_overlaps + partial_overlaps_strict
-            first_assignment_range  = range(first_assignment_start,first_assignment_stop+1)
-            second_assignment_range = range(second_assignment_start,second_assignment_stop+1)
             if any(first_section in second_assignment_range for first_section in first_assignment_range):
                 partial_overlaps += 1
     print(f'{complete_overlaps = }')
