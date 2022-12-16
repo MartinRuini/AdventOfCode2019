20e582d147f12e233d5acef89452866d42497395
druini <daniele.ruini@cern.ch>
2022-12-16 16:04:25 +0100

    finish 2022/13

--- 2022/13/day13.py
+++ 2022/13/day13.py
@@ -1,3 +1,5 @@
+import itertools
+
 INPUT = 'input.txt'
 
 def compare(left, right):
@@ -40,9 +42,46 @@ def find_valid_pairs(input_file):
             yield pair_idx
 
 
+def find_index(packet_list, packet):
+    if len(packet_list)==0:
+        return 0
+    elif len(packet_list)==1:
+        idx = 1 if compare(packet_list[0], packet) else 0
+        return idx
+    else:
+        half_length = len(packet_list)//2
+        if compare(packet, packet_list[half_length]):
+            idx = find_index(packet_list[:half_length], packet)
+            return idx
+        else:
+            idx_second_half = find_index(packet_list[half_length:], packet)
+            return half_length+idx_second_half
+
+
+def sort_packets(input_file):
+    sorted_packets = []
+    with open(input_file) as input_data:
+        for line in itertools.chain(input_data.readlines(), ['[[2]]', '[[6]]']):
+            line = line.strip()
+            if line:
+                packet = eval(line)
+                idx = find_index(sorted_packets, packet)
+                sorted_packets.insert(idx, packet)
+                if packet in ([[2]], [[6]]):
+                    yield idx
+    #return sorted_packets
+
+
 def main():
+    #part 1
     valid_pairs = find_valid_pairs(INPUT)
     print(f'{sum(valid_pairs) = }')
+
+    #part 2
+    decoder_key = 1
+    for idx in sort_packets(INPUT):
+        decoder_key *= idx+1
+    print(f'{decoder_key = }')
 
 
 if __name__=='__main__':
